2f9448b49ad292f39fd249771e454e7413fd6e56
Dan Colish <dcolish@gmail.com>
2011-04-16 22:58:04 -0700

    clean up warnings

--- xapian-bindings/ocaml/smoketest.ml
+++ xapian-bindings/ocaml/smoketest.ml
@@ -1,42 +1,20 @@
-open Swig;;
-open Xapian;;
-
-(* Helpers *)
-let get_closure obj = 
-    match obj with
-        C_list [C_string a; C_obj f] -> C_obj f
-      | _ -> raise (Failure "ZOMG")
-;;
-
-let parse_query qp query = 
-  let c_query = C_string query in
-    get_closure (qp -> parse_query(c_query))
-;;
-
-let writable_db path = 
-  let c_path = C_string path in 
-    get_closure (new_WritableDatabase '(c_path, 1))
-;;
-
-let add_terms doc terms =
-  List.map (fun el -> 
-              let c_el = C_string el in
-                doc -> add_term(c_el)) terms
-;;
+open Swig
+open Xapian
+open Unix
 
 (* Quick test to ensure the module is loaded *)
 let _ = 
   assert (compare Xapian.module_name "xapian" == 0);
-  assert (compare (_version_string '() as string) "1.2.5" == 0)
+  assert (compare (Xapian.version_string '() as string) "1.2.5" == 0)
 ;;
 
 (* Basis document test *)
 
 let _ = 
   let doc = new_Document '() in
-    add_terms doc ["hello"];
+    ignore (add_terms doc ["hello"]);
     assert ((doc -> termlist_count() as int) == 1);
-    doc -> set_data ("something");
+    ignore (doc -> set_data ("something"));
     assert (compare (doc -> get_data() as string) "something" == 0);
     let tb = doc -> termlist_begin() in
     let te = doc -> termlist_end() in
@@ -46,19 +24,27 @@ let _ =
 
 (* simple database test *)
 let _ = 
-  let db = writable_db "test.db" in
+  let db = writable_db "test.db" 1 in
   let doc = new_Document '() in
-    add_terms doc ["hello"];
-    doc -> set_data("goodbye");
-    db -> add_document(doc);
-    db -> flush();
+    ignore (add_terms doc ["hello"]);
+    ignore (doc -> set_data("goodbye"));
+    ignore (db -> add_document(doc));
+    ignore (db -> flush());
     let qp = new_QueryParser '() in 
-      qp -> set_database(db);
-      let pq = parse_query qp "hello" in
+      ignore (qp -> set_database(db));
+      let pq = build_parsed_query qp "hello" in
       let tb = pq -> get_terms_begin() in
         assert (pq -> empty() as bool == false);
         assert (compare (tb -> get_term() as string) "hello" == 0)
 ;;
 
+
+let _ = 
+  ignore (
+    Array.map (fun el ->
+                 ignore (Sys.remove (Filename.concat "test.db" el)))
+      (Sys.readdir "test.db"));
+  Unix.rmdir "test.db"
+;;
 
 print_endline "All tests passed."
---
+++ xapian-bindings/ocaml/util.i
@@ -0,0 +1,38 @@
+%insert (mlitail) %{
+    val version_string : c_obj -> c_obj
+
+    val closure_from_lst : c_obj -> c_obj
+    val build_parsed_query : c_obj -> string -> c_obj
+    val writable_db : string -> int -> c_obj
+    val add_terms : c_obj -> string list -> c_obj list
+
+%}
+
+%insert (mltail) %{
+
+let version_string = _version_string
+(* Helpers *)
+
+let closure_from_lst lst = 
+    match lst with
+        Swig.C_list [Swig.C_string a; Swig.C_obj f] -> Swig.C_obj f
+      | _ -> raise (Failure "ZOMG")
+
+
+let build_parsed_query qp query = 
+  let c_query = Swig.C_string query in
+    closure_from_lst ((invoke qp) "parse_query" (c_query))
+
+
+let writable_db path flags = 
+  let c_path = Swig.C_string path in 
+    let c_flags = Swig.C_int flags in
+    closure_from_lst (new_WritableDatabase (Swig.C_list[c_path; c_flags]))
+
+
+let add_terms doc terms =
+  List.map (fun el -> 
+              let c_el = Swig.C_string el in
+                (invoke doc) "add_term" (c_el)) terms
+
+%}
